6e6d460d45c6e130bb477621ffec770d0d81f0f8
sangam shinde <sangampshinde@gmail.com>
2024-07-18 19:00:06 +0530

    project setup

---
+++ frontend/src/data.js
@@ -0,0 +1,26 @@
+export const data =[
+    {
+        id:"1",
+        group:"test",
+        image:["images1", "images2", "images"],
+        title:"titles",
+        github:"github",
+        demo:"demo"
+    },
+    {
+        id:"2",
+        group:"test2",
+        image:["images1", "images2", "images"],
+        title:"titles",
+        github:"github",
+        demo:"demo"
+    },
+    {
+        id:"3",
+        group:"test3",
+        image:["images1", "images2", "images"],
+        title:"titles",
+        github:"github",
+        demo:"demo"
+    }
+]

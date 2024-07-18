fb0ab595077b875bda43c822556131a78ac37a36
sangam shinde <sangampshinde@gmail.com>
2024-07-19 00:06:26 +0530

    designing about component

--- frontend/src/About Me/About.js
+++ frontend/src/About Me/About.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import "../App.css";
 
-const About = () => {
+const About = ({load}) => {
   return (
-    <div className = {`project__item ${load ? "zoom_in":""}`} key={index}>
+    <div className = {`project__item ${load ? "zoom_in":""}`}>
                 <div className="project_item_image">
                 
-                <img src={image} alt={title}/>
+                <img src={()=>{}} alt={()=>{}}/>
                 </div>
-                <h3>{title}</h3>
+                <h3>{()=>{}}</h3>
                 <div className='project_item_button'>
-                <a href={github} className='btn ' target="_blank">GitHub</a>
-                <a href={demo}  className='btn btn__primary' target="_blank"> Live Demo</a>  
+                <a href={()=>{}} className='btn ' target="_blank">GitHub</a>
+                <a href={()=>{}}  className='btn btn__primary' target="_blank"> Live Demo</a>  
                 </div>
    </div>
   )
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { data } from './data';
 import NavTab from './NavTab/NavTab';
+import About from './About Me/About';
 
 function App() {
 
@@ -38,6 +39,7 @@ function App() {
   return (
     <div className="App">
      <button className='btn btn__primary about'>About Me</button>
+      <About load={load}/> 
       <NavTab
       tabs={getTabs()}
       onChange={setProject}

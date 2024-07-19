737a6968f9850e989fd3cb86af4ea3f9ff5ab42e
sangam shinde <sangampshinde@gmail.com>
2024-07-19 22:29:19 +0530

    creating about and home page

--- frontend/src/About Me/About.js
+++ frontend/src/About Me/About.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import "../App.css";
 
-const About = ({load}) => {
+const About = () => {
   return (
-    <div className = {`project__item ${load ? "zoom_in":""}`}>
-                <div className="project_item_image">
-                
-                <img src={()=>{}} alt={()=>{}}/>
-                </div>
-                <h3>{()=>{}}</h3>
-                <div className='project_item_button'>
-                <a href={()=>{}} className='btn ' target="_blank">GitHub</a>
-                <a href={()=>{}}  className='btn btn__primary' target="_blank"> Live Demo</a>  
-                </div>
-   </div>
+    <div>
+
+    </div>
+   
   )
 }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,16 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { data } from './data';
 import NavTab from './NavTab/NavTab';
+import { BrowserRouter as Router, Routes, Route ,Link} from "react-router-dom";
+import About from "./About Me/About";
+
+const about = (
+  <Router>
+    <Routes>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+  </Router>
+)
 
 
 function App() {
@@ -36,37 +46,35 @@ function App() {
   };
 
 
-  return (
-    <div className="App">
-     <button className='btn btn__primary about'>About Me</button>
-    
-      <NavTab
-      tabs={getTabs()}
-      onChange={setProject}
-      />
-      <div className='project__container'>
-        
-        {
-          projects.map(({image, title, github,demo},index)=>{
-            return(
-              <div className = {`project__item ${load ? "zoom_in":""}`} key={index}>
+    return (
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <button className='btn btn__primary about'>
+            <Link to="/about">About Me</Link>
+          </button>
+          
+          <NavTab tabs={getTabs()} onChange={setProject} />
+          
+          <div className='project__container'>
+            {projects.map(({ image, title, github, demo }, index) => (
+              <div className={`project__item ${load ? "zoom_in" : ""}`} key={index}>
                 <div className="project_item_image">
-                  <img src={image} alt={title}/>
+                  <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
                 <div className='project_item_button'>
-                <a href={github} className='btn ' target="_blank">GitHub</a>
-                <a href={demo}  className='btn btn__primary' target="_blank"> Live Demo</a>  
+                  <a href={github} className='btn' target="_blank" rel="noopener noreferrer">GitHub</a>
+                  <a href={demo} className='btn btn__primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
                 </div>
               </div>
-            )
-
-          })
-        }
-
-      </div>
-    </div>
-  );
-}
+            ))}
+          </div>
+        </div>
+      </Router>
+    );
+  }
 
 export default App;
